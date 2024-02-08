f9b03a4c00b678a7c0abb78e0d3f2882d3df4157
dreamagici4n <lucascostamr812@gmail.com>
2024-02-08 16:52:13 -0300

    test: ensure TransactionController returns status on success

--- src/controllers/transaction.spec.js
+++ src/controllers/transaction.spec.js
@@ -3,16 +3,23 @@ const TransactionController = require('./trasaction.js')
 const makeAddTransactionRepository = () => {
     class AddTransactionRepositoryStub {
         async add() {
-            return new Promise(resolve => resolve('Transaction added'))
+            return new Promise(resolve => resolve(makeFakeStatus()))
         }
     }
     return new AddTransactionRepositoryStub()
 }
 
+const makeFakeStatus = () => ({
+    "limite" : 100000,
+    "saldo" : -9098
+})
+
 const makeFakeRequest = () => ({
-    valor: "any_valor",
-    tipo: "any_tipo",
-    descricao: "descricao"
+    body: {
+        valor: "any_valor",
+        tipo: "any_tipo",
+        descricao: "descricao"
+    }
 })
 
 const makeSut = () => {
@@ -25,40 +32,60 @@ const makeSut = () => {
 }
 
 describe('Transaction Controller', () => {
-    test('Should return 400 if valor is not provided', () => {
+    test('Should return 400 if valor is not provided', async () => {
         const { sut } = makeSut()
         const httpRequest = {
-            tipo: "any_tipo",
-            descricao: "any_descricao"
+            body: {
+                tipo: "any_tipo",
+                descricao: "any_descricao"
+            }
         }
-        const httpResponse = sut.handle(httpRequest)
-        expect(httpResponse).toBe(400)
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
     })
 
-    test('Should return 400 if tipo is not provided', () => {
+    test('Should return 400 if tipo is not provided', async () => {
         const { sut } = makeSut()
         const httpRequest = {
-            valor: "any_valor",
-            descricao: "any_descricao"
+            body: {
+                valor: "any_valor",
+                descricao: "any_descricao"
+            }
         }
-        const httpResponse = sut.handle(httpRequest)
-        expect(httpResponse).toBe(400)
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
     })
 
-    test('Should return 400 if descricao is not provided', () => {
+    test('Should return 400 if descricao is not provided', async () => {
         const { sut } = makeSut()
         const httpRequest = {
-            valor: "any_valor",
-            tipo: "any_tipo"
+            body: {
+                valor: "any_valor",
+                tipo: "any_tipo"
+            }
         }
-        const httpResponse = sut.handle(httpRequest)
-        expect(httpResponse).toBe(400)
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
     })
     
-    test('Should call AddTransactionRepository with correct values', () => {
+    test('Should call AddTransactionRepository with correct values', async () => {
         const { sut, addTransactionRepositoryStub } = makeSut()
         const addSpy = jest.spyOn(addTransactionRepositoryStub, 'add')
-        sut.handle(makeFakeRequest())
-        expect(addSpy).toHaveBeenCalledWith(makeFakeRequest())
+        await sut.handle(makeFakeRequest())
+        expect(addSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+    })
+
+    test('Should return 500 if AddTransactionRepository throws', async () => {
+        const { sut, addTransactionRepositoryStub } = makeSut()
+        jest.spyOn(addTransactionRepositoryStub, 'add').mockRejectedValueOnce(new Error())
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse.statusCode).toBe(500)
+    })
+
+    test('Should return status on success', async () => {
+        const { sut } = makeSut()
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse.statusCode).toBe(200)
+        expect(httpResponse.body).toEqual(makeFakeStatus())
     })
 });
