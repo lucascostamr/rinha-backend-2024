8ac78a536c5f458332c8b17ac1b87b64784b9eba
dreamagici4n <lucascostamr812@gmail.com>
2024-02-08 17:10:49 -0300

    test: ensure AddTransactionRepository calls TransactionRepository with correct values

---
+++ src/model/data/add-transaction-repository.spec.js
@@ -0,0 +1,34 @@
+const AddTransactionRepository = require('./add-transaction-repository')
+
+const makeAddRepository = () => {
+    class TransactionRepositoryStub {
+        async add () {
+            return new Promise(resolve => resolve(''))
+        }
+    }
+    return new TransactionRepositoryStub()
+}
+
+const makeFakeTransaction = () => ({
+    valor: "any_valor",
+    tipo: "any_tipo",
+    descricao: "descricao"
+})
+
+const makeSut = () => {
+    const transactionRepositoryStub = makeAddRepository()
+    const sut = new AddTransactionRepository(transactionRepositoryStub)
+    return {
+        sut,
+        transactionRepositoryStub
+    }
+}
+
+describe('Add Transaction Repository', () => {
+    test('Should call TransactionRepository with correct values', async () => {
+        const { sut, transactionRepositoryStub } = makeSut()
+        const addSpy = jest.spyOn(transactionRepositoryStub, 'add')
+        await sut.add(makeFakeTransaction())
+        expect(addSpy).toHaveBeenCalledWith(makeFakeTransaction())
+    })
+});
