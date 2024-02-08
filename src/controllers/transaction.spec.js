2fff38a11b8b75b32cea9ee5d0cbaba4185fa3f6
dreamagici4n <lucascostamr812@gmail.com>
2024-02-08 16:36:10 -0300

    refactor: move sut and AddTransactionRepository to a factory method

--- src/controllers/transaction.spec.js
+++ src/controllers/transaction.spec.js
@@ -1,66 +1,64 @@
 const TransactionController = require('./trasaction.js')
 
+const makeAddTransactionRepository = () => {
+    class AddTransactionRepositoryStub {
+        async add() {
+            return new Promise(resolve => resolve('Transaction added'))
+        }
+    }
+    return new AddTransactionRepositoryStub()
+}
+
+const makeFakeRequest = () => ({
+    valor: "any_valor",
+    tipo: "any_tipo",
+    descricao: "descricao"
+})
+
+const makeSut = () => {
+    const addTransactionRepositoryStub = makeAddTransactionRepository()
+    const sut = new TransactionController(addTransactionRepositoryStub)
+    return {
+        sut,
+        addTransactionRepositoryStub
+    }
+}
+
 describe('Transaction Controller', () => {
     test('Should return 400 if valor is not provided', () => {
-        const sut = new TransactionController()
-
+        const { sut } = makeSut()
         const httpRequest = {
             tipo: "any_tipo",
             descricao: "any_descricao"
         }
-
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse).toBe(400)
     })
 
     test('Should return 400 if tipo is not provided', () => {
-        const sut = new TransactionController()
-
+        const { sut } = makeSut()
         const httpRequest = {
             valor: "any_valor",
             descricao: "any_descricao"
         }
-
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse).toBe(400)
     })
 
     test('Should return 400 if descricao is not provided', () => {
-        const sut = new TransactionController()
-
+        const { sut } = makeSut()
         const httpRequest = {
             valor: "any_valor",
             tipo: "any_tipo"
         }
-
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse).toBe(400)
     })
     
     test('Should call AddTransactionRepository with correct values', () => {
-        class AddTransactionRepositoryStub {
-            async add() {
-                return new Promise(resolve => resolve('Transaction added'))
-            }
-        }
-        
-        const addTransactionRepositoryStub = new AddTransactionRepositoryStub()
-        const sut = new TransactionController(addTransactionRepositoryStub)
-
+        const { sut, addTransactionRepositoryStub } = makeSut()
         const addSpy = jest.spyOn(addTransactionRepositoryStub, 'add')
-
-        const httpRequest = {
-            valor: "any_valor",
-            tipo: "any_tipo",
-            descricao: "descricao"
-        }
-
-        sut.handle(httpRequest)
-
-        expect(addSpy).toHaveBeenCalledWith({
-            valor: "any_valor",
-            tipo: "any_tipo",
-            descricao: "descricao"
-        })
+        sut.handle(makeFakeRequest())
+        expect(addSpy).toHaveBeenCalledWith(makeFakeRequest())
     })
 });
