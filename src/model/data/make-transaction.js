2c3bb5cb60c6c24c880b85d007aabc7e8e590165
dreamagici4n <lucascostamr812@gmail.com>
2024-02-09 19:14:45 -0300

    feat: ensure MakeTransaction do the logic and returns the correct status

--- src/model/data/make-transaction.js
+++ src/model/data/make-transaction.js
@@ -1,13 +1,26 @@
 class MakeTransaction {
+    _limite
+    _saldo
     constructor(clientRepository) {
         this.clientRepository = clientRepository
     }
 
     async make (transaction) {
-        const client = await this.clientRepository.get(transaction.client_id)
+        const { client_id, valor, tipo } = transaction
+        const { limite, saldo_inicial} = await this.clientRepository.get(client_id)
+        switch (tipo) {
+            case "c":
+                this._limite = +limite - (+valor)
+                this._saldo = saldo_inicial
+                break;
+            case "d":
+                this._saldo = +saldo_inicial - (+valor)
+                this._limite = limite
+                break;
+        }
         return {
-            limite: "any_limite",
-            saldo: "any_saldo"
+            limite: this._limite,
+            saldo: this._saldo
         };
     }
 }
