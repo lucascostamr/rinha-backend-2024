4f262e289a72fdf92a595aaaec56ce1e1f680f4a
dreamagici4n <lucascostamr812@gmail.com>
2024-02-12 15:44:06 -0300

    ensure UpdateClientRepository returns macthedCount on success

--- src/infra/database/mongo/client/update-client-mongo-repository.js
+++ src/infra/database/mongo/client/update-client-mongo-repository.js
@@ -4,10 +4,9 @@ const MongoHelper = require('../helpers/mongo-helper')
 class UpdateClientRepository {
     async update(client) {
         if(!client) return new Promise((resolve, reject) => reject(new MissingParamError('client')))
-        // const clientCollection = MongoHelper.getCollection('clients')
-        // const client = await clientCollection.findOne({ id: clientId })
-        // if(!client) throw new ClientNotFoundError(clientId)
-        return new Promise(resolve => resolve({ matchedCount: 1}))
+        const { id, limite, saldo } = client
+        const clientCollection = MongoHelper.getCollection('clients')
+        return await clientCollection.updateOne({ id: id }, { $set: { limite: limite, saldo_inicial: saldo }})
     }
 }
 
