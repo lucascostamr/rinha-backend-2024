a6902f1d62d93fd70e45fbfc4d0b5e92efae7538
dreamagici4n <lucascostamr812@gmail.com>
2024-02-12 20:06:53 -0300

    test: ensure SaveTransactionModel calls AddTransactionRepository with correct values

--- src/model/data/transaction/save-transaction.spec.js
+++ src/model/data/transaction/save-transaction.spec.js
@@ -1,24 +1,52 @@
 const SaveTransactionModel = require('./save-transaction')
 
-describe('Save Transaction', () => {
-  test('Should return client last transactions on success', async () => {
-    const sut = new SaveTransactionModel()
-    const transaction = {
+const makeAddTransactionStub = () => {
+  class AddTransactionRepositoryStub {
+    async add () {
+      return new Promise(resolve => resolve(makeFakeLastTransactions()))
+    }
+  }
+  return new AddTransactionRepositoryStub() 
+}
+
+const makeFakeTransaction = () => ({
+  valor: 'any_value',
+  tipo: 'any_tipo',
+  descricao: 'any_descricao',
+  realizada_em: new Date('2024-02-12')
+})
+
+const makeFakeLastTransactions = () => ({
+  ultimas_trasacoes: [
+    {
       valor: 'any_value',
       tipo: 'any_tipo',
       descricao: 'any_descricao',
       realizada_em: new Date('2024-02-12')
     }
-    const response = await sut.save(transaction)
-    expect(response).toEqual({
-      ultimas_trasacoes: [
-        {
-          valor: 'any_value',
-          tipo: 'any_tipo',
-          descricao: 'any_descricao',
-          realizada_em: new Date('2024-02-12')
-        }
-      ]
-    })
+  ]
+})
+
+const makeSut = () => {
+  const addTransactionRepositoryStub = makeAddTransactionStub()
+  const sut = new SaveTransactionModel(addTransactionRepositoryStub)
+  return {
+    sut,
+    addTransactionRepositoryStub
+  }
+}
+
+describe('Save Transaction', () => {
+  test('Should call AddTransactionStub with correct values', async () => {
+    const { sut, addTransactionRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addTransactionRepositoryStub, 'add')
+    await sut.save(makeFakeTransaction())
+    expect(addSpy).toHaveBeenCalledWith(makeFakeTransaction())
+  })
+  
+  test('Should return client last transactions on success', async () => {
+    const { sut } = makeSut()
+    const response = await sut.save(makeFakeTransaction())
+    expect(response).toEqual(makeFakeLastTransactions())
   })
 });
