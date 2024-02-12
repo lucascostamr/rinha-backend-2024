e5d62ed41fb3e01434f6b5fd03db685a33eea07c
dreamagici4n <lucascostamr812@gmail.com>
2024-02-12 15:37:36 -0300

    feat: ensure UpdateClientRepository throws if no client is provided

--- src/infra/database/mongo/client/update-client-mongo-repository.spec.js
+++ src/infra/database/mongo/client/update-client-mongo-repository.spec.js
@@ -1,5 +1,33 @@
+const MissingParamError = require('../../../../errors/missing-param-error');
+const MongoHelper = require('../helpers/mongo-helper');
+const UpdateClientRepository = require('./update-client-mongo-repository');
+
 describe('Update Client Mongo Repository', () => {
-  test('', () => {
-    
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URI)
+  })
+
+  beforeEach( async () => {
+      const clientCollection = MongoHelper.getCollection('clients')
+      await clientCollection.insertOne({
+          id: 1,
+          limite: 100000,
+          saldo_inicial: 0
+      })
+  })
+
+  afterEach(async () => {
+      const clientCollection = MongoHelper.getCollection('clients')
+      await clientCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+      await MongoHelper.disconnect()
+  })
+  
+  test('Should throw if no client is provided', async () => {
+    const sut = new UpdateClientRepository()
+    const response = sut.update()
+    await expect(response).rejects.toThrow(new MissingParamError('client'))
   })
 });
