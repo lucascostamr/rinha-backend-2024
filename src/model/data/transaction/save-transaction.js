be14ad2aea0a8ce95fa728516aaf7cf707c9de0a
dreamagici4n <lucascostamr812@gmail.com>
2024-02-12 20:11:18 -0300

    feat: ensure SaveTransactionModel returns last transactions if AddTransactionRepository returns last transactions

--- src/model/data/transaction/save-transaction.js
+++ src/model/data/transaction/save-transaction.js
@@ -4,17 +4,8 @@ class SaveTransactionModel {
   }
 
   async save(transaction) {
-    await this.addTransactionStub.add(transaction)
-    return {
-      ultimas_trasacoes: [
-        {
-          valor: 'any_value',
-          tipo: 'any_tipo',
-          descricao: 'any_descricao',
-          realizada_em: new Date('2024-02-12')
-        }
-      ]
-    }
+    const lastTransactions = await this.addTransactionStub.add(transaction)
+    return lastTransactions
   }
 }
 module.exports = SaveTransactionModel
