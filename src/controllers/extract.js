11788645954c3f19b2eaaad67da15d2690336d8d
dreamagici4n <lucascostamr812@gmail.com>
2024-02-13 20:41:52 -0300

    feat: ensure ExtractController calls GetClientRepository with correct client_id

--- src/controllers/extract.js
+++ src/controllers/extract.js
@@ -1,6 +1,12 @@
 class ExtractController {
+  getClientRepository
+
+  constructor(getClientRepository) {
+    this.getClientRepository = getClientRepository
+  }
   async handle (httpRequest) {
     if(!httpRequest.body.client_id) return { statusCode: 400 }
+    await this.getClientRepository.get(httpRequest.body.client_id)
   }
 }
 
