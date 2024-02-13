4f44ab5514734c346186d337c40ecd0a54290ef1
dreamagici4n <lucascostamr812@gmail.com>
2024-02-13 14:53:29 -0300

    feat: esnure Cors is enable

--- src/main/config/middleware.js
+++ src/main/config/middleware.js
@@ -1,5 +1,7 @@
 const bodyParser = require('../middlewares/body-parser')
+const cors = require('../middlewares/cors')
 
 module.exports = (app) => {
   app.use(bodyParser)
+  app.use(cors)
 }
--- src/main/middlewares/body-parser.spec.js
+++ src/main/middlewares/body-parser.spec.js
@@ -2,11 +2,11 @@ const app = require('../config/app')
 const request = require('supertest')
 
 describe('Body', () => {
-  test('Should parse to json', () => {
+  test('Should parse to json', async () => {
     app.post('/test_body_parser', (req, res) => {
       res.send(req.body)
     })
-    request(app)
+    await request(app)
     .post('/test_body_parser')
     .send({ name: "any_name" })
     .expect({ name: "any_name" })
