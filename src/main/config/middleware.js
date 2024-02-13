858a0e217099b79eb2aa98697f499cb903df90d1
dreamagici4n <lucascostamr812@gmail.com>
2024-02-13 15:00:27 -0300

    feat: ensure ContentType return json as default and xml if forced

--- src/main/config/middleware.js
+++ src/main/config/middleware.js
@@ -1,7 +1,9 @@
 const bodyParser = require('../middlewares/body-parser')
 const cors = require('../middlewares/cors')
+const contentType = require('../middlewares/content-type')
 
 module.exports = (app) => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
