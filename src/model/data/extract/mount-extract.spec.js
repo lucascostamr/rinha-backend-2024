3d4b25ed8397c32841926663447d4f04e98f769e
dreamagici4n <lucascostamr812@gmail.com>
2024-02-14 20:40:27 -0300

    test: ensure MountExtractModel returns null if no saldo_inicial is provided

--- src/model/data/extract/mount-extract.spec.js
+++ src/model/data/extract/mount-extract.spec.js
@@ -6,4 +6,15 @@ describe('Mount Extract Model', () => {
     const response = sut.mount()
     expect(response).toBe(null)
   })
+
+  test('Should return null if no saldo_inicial is provided', () => {
+    const sut = new MountExtractModel()
+    const client = {
+      saldo_inicial: 'any_saldo',
+      limite: 'any_limite',
+      ultimas_transacoes: []
+    }
+    const response = sut.mount()
+    expect(response).toBe(null)
+  })
 });
