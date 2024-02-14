df0cbfb009aff57cac59a145edf17e49543c9881
dreamagici4n <lucascostamr812@gmail.com>
2024-02-14 20:44:29 -0300

    feat: ensure MountExtractModel returns null if no ultimas_transacoes is provided

--- src/model/data/extract/mount-extract.js
+++ src/model/data/extract/mount-extract.js
@@ -1,7 +1,7 @@
 class MountExtractModel {
   mount (client) {
     if(!client) return null
-    for(const field of ['saldo_inicial', 'limite']) {
+    for(const field of ['saldo_inicial', 'limite', 'ultimas_transacoes']) {
       if(!client[field]) return null
     }
   }
