40b97dd63e05993d13f84cbc075c31a756ae5894
dreamagici4n <lucascostamr812@gmail.com>
2024-02-13 21:11:42 -0300

    test: ensure ExtractController returns 500 if MountExtract throws

--- src/controllers/extract.spec.js
+++ src/controllers/extract.spec.js
@@ -1,6 +1,8 @@
+const ClientNotFoundError = require('../errors/client-not-found')
+const { clientNotFoundError, serverError } = require('../helpers/http')
 const ExtractController = require('./extract')
 
-const makeGetClientRepositoryStub = () => {
+const makeGetClientRepository = () => {
   class GetClientRepositoryStub {
     async get(clientId) {
       return new Promise(resolve => resolve('client')) 
@@ -8,12 +10,30 @@ const makeGetClientRepositoryStub = () => {
   }
   return new GetClientRepositoryStub()
 }
+
+const makeMountExtract = () => {
+  class MountExtractStub {
+    async mount () {
+      return new Promise(resolve => resolve('extract'))
+    }
+  }
+  return new MountExtractStub()
+}
+
+const makeFakeRequest = () => ({
+  body: {
+    client_id: 'any_id'
+  }
+})
+
 const makeSut = () => {
-  const getClientRepositoryStub = makeGetClientRepositoryStub()
-  const sut = new ExtractController(getClientRepositoryStub)
+  const mountExtractStub = makeMountExtract()
+  const getClientRepositoryStub = makeGetClientRepository()
+  const sut = new ExtractController(getClientRepositoryStub, mountExtractStub)
   return {
     sut,
-    getClientRepositoryStub
+    getClientRepositoryStub,
+    mountExtractStub
   }
 }
 describe('Extract Controller', () => {
@@ -29,12 +49,28 @@ describe('Extract Controller', () => {
   test('Should call getClientRepository with correct client_id', async () => {
     const { sut, getClientRepositoryStub } = makeSut()
     const getSpy = jest.spyOn(getClientRepositoryStub, 'get')
-    const httpRequest = {
-      body: {
-        client_id: 'any_id'
-      }
-    }
-    await sut.handle(httpRequest)
+    await sut.handle(makeFakeRequest())
     expect(getSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('Should return 404 if getClientRepository throws', async () => {
+    const { sut, getClientRepositoryStub } = makeSut()
+    jest.spyOn(getClientRepositoryStub, 'get').mockRejectedValueOnce(new ClientNotFoundError())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(clientNotFoundError(new ClientNotFoundError()))
+  })
+
+  test('Should call MountExtract with correct values', async () => {
+    const { sut, mountExtractStub } = makeSut()
+    const mountSpy = jest.spyOn(mountExtractStub, 'mount')
+    await sut.handle(makeFakeRequest())
+    expect(mountSpy).toHaveBeenCalledWith('client')
+  })
+
+  test('Should return 500 if MountExtract throws', async () => {
+    const { sut, mountExtractStub } = makeSut()
+    jest.spyOn(mountExtractStub, 'mount').mockRejectedValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 });
