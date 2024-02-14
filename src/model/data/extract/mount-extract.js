e27477d1029f6b148f8899a2732fbdbe532bacce
dreamagici4n <lucascostamr812@gmail.com>
2024-02-14 20:53:01 -0300

    feat: ensure MountExtractModel returns extract on success

--- src/model/data/extract/mount-extract.js
+++ src/model/data/extract/mount-extract.js
@@ -4,6 +4,16 @@ class MountExtractModel {
     for(const field of ['saldo_inicial', 'limite', 'ultimas_transacoes']) {
       if(!client[field]) return null
     }
+    const { saldo_inicial, limite, ultimas_transacoes } = client
+    const extract = {
+      saldo: {
+        total: saldo_inicial,
+        data_extrato: new Date(),
+        limite: limite
+      },
+      ultimas_transacoes
+    }
+    return extract
   }
 }
 
